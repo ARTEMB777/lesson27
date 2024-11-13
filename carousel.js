f08f8927bd01f29bbae6339492af9ad0b4903b8a
ARTEMB <artem.bosyi@gmail.com>
2024-11-13 21:32:25 +0200

    переписано код з використанням класів

--- carousel.js
+++ carousel.js
@@ -1,27 +1,35 @@
-function Carousel(containerId = '#carousel', slideId = 'slide', interval = 2000, isPlaying = true){
-  this.container = document.querySelector(containerId);
-  this.slides = document.querySelectorAll(slideId);
-  this.INTERVAL = interval; // Інтервал між слайдами в мілісекундах
-  this.isPlaying = isPlaying;
-}
+class Carousel{
+  constructor (params){
+    const settings = {...{
+      containerId: '#carousel',
+      slideId: '.slide',
+      interval: 2000,
+      isPlaying: true,
+    },...params }
 
-Carousel.prototype = {
+    this.container = document.querySelector(settings.containerId);
+    this.slideItems = document.querySelectorAll(settings.slideId);
+    this.INTERVAL = settings.interval; // Інтервал між слайдами в мілісекундах
+    this.isPlaying = settings.isPlaying;
+  }
+
   _initProps(){
     this.currentSlide = 0;
-    this.slideInterval = setInterval(this._nextSlide.bind(this), this.INTERVAL);
     
-    this.SLIDES_COUNT = this.slides.length
+    this.SLIDES_COUNT = this.slideItems.length
     this.CODE_ARROW_LEFT = 'ArrowLeft'
     this.CODE_ARROW_RIGHT = 'ArrowRight'
     this.CODE_SPACE = 'Space'
-  },
+  }
 
   _initControls(){
     const controls = document.createElement('div')
-    const PAUSE = '<button class="control" id="pause"></button>'
+    const PAUSE = `<button class="control" id="pause">
+      ${this.isPlaying ? 'Pause' : 'Play'}
+    </button>`
     const PREV = '<button class="control" id="previous"></button>'
     const NEXT = '<button class="control" id="next"></button>'
-
+      
     controls.innerHTML = PAUSE + PREV + NEXT
 
     controls.setAttribute('id', 'controls-container')
@@ -31,7 +39,7 @@ Carousel.prototype = {
     this.pauseButton = document.querySelector('#pause');
     this.nextButton = document.querySelector('#next');
     this.prevButton = document.querySelector('#previous');
-  },
+  }
 
   _initIndicators(){
     const indicators = document.createElement('div')
@@ -50,7 +58,7 @@ Carousel.prototype = {
 
     this.indicatorsContainer = document.querySelector('#indicators-container')
     this.indicatorsItems = document.querySelectorAll('.indicator')
-  },
+  }
 
   _initListeners(){
     this.pauseButton.addEventListener('click', this.pausePlay.bind(this));
@@ -58,25 +66,32 @@ Carousel.prototype = {
     this.prevButton.addEventListener('click', this.previous.bind(this));
     this.indicatorsContainer.addEventListener('click', this._indicate.bind(this));
     document.addEventListener('keydown', this._pressKey.bind(this));
-  },
+    this.container.addEventListener('mouseenter', this.pause.bind(this));
+    this.container.addEventListener('mouseleave', this.play.bind(this));
+  }
 
 // Функція для переходу до конкретного слайду
   _goToSlide(n) {
-    this.slides[this.currentSlide].classList.remove('active'); // Прибираємо клас 'active' у поточного слайду
+    this.slideItems[this.currentSlide].classList.remove('active'); // Прибираємо клас 'active' у поточного слайду
     this.indicatorsItems[this.currentSlide].classList.remove('active'); // Прибираємо клас 'active' у поточного слайду
     this.currentSlide = (n + this.SLIDES_COUNT) % this.SLIDES_COUNT; // Вираховуємо новий поточний слайд
-    this.slides[this.currentSlide].classList.add('active'); // Додаємо клас 'active' новому слайду
+    this.slideItems[this.currentSlide].classList.add('active'); // Додаємо клас 'active' новому слайду
     this.indicatorsItems[this.currentSlide].classList.add('active'); // Додаємо клас 'active' новому слайду
-  },
+  }
 
   // Функція для переходу до наступного або попереднього слайду
   _nextSlide() {
     this._goToSlide(this.currentSlide + 1); // Переходимо до наступного слайду
-  },
+  }
 
   _previousSlide() {
     this._goToSlide(this.currentSlide - 1); // Переходимо до попереднього слайду
-  }, 
+  }
+
+  _tick() {
+    if (!this.isPlaying || this.slideInterval) return; // Перевіряємо, чи не встановлено значення `false` для isPlaying
+    this.slideInterval = setInterval(() => this._nextSlide(), this.INTERVAL);
+  }
   
   _indicate(e){
   const {target} = e
@@ -85,7 +100,7 @@ Carousel.prototype = {
     this.pause()
     this._goToSlide(+target.dataset.slideTo)
     }
-  },
+  }
 
   _pressKey(e){
     const { code } = e
@@ -95,44 +110,50 @@ Carousel.prototype = {
       e.preventDefault()
       this.pausePlay ()
     } 
-  },
+  }
 
   // Функція для паузи автопрогравання слайдера
   pause() {
     this.pauseButton.innerHTML = 'Play'; // Змінюємо текст кнопки на "Play"
     this.isPlaying = false;
     clearInterval(this.slideInterval); // Зупиняємо інтервал автопрогравання
-  },
+    this.slideInterval = null
+  }
 
   // Функція для відновлення автопрогравання слайдера
   play() {
+    if(this.isPlaying) return
     this.pauseButton.innerHTML = 'Pause'; // Змінюємо текст кнопки на "Pause"
     this.isPlaying = true;
-    this.slideInterval = setInterval(this._nextSlide.bind(this), this.INTERVAL); // Відновлюємо інтервал автопрогравання
-  },
+    this._tick();
+  }
 
   // Встановлення функції на кнопку паузи, яка перемикає між паузою та відтворенням
   pausePlay() {
     this.isPlaying ? this.pause() : this.play();
-  },
+  }
 
   // Зупинка автопрогравання та перехід до наступного або попереднього слайду при натисканні кнопок
   next() {
     this.pause();
     this._nextSlide();
-  },
+  }
 
   previous() {
    this.pause();
     this._previousSlide();
-  },
+  }
 
   init(){
+    this. _tick()
     this._initProps()
     this._initControls()
     this._initIndicators()
     this._initListeners()
+    
   }
 }
 
-Carousel.prototype.constructor = Carousel
+export default Carousel
+
+
--- swipe-carousel.js
+++ swipe-carousel.js
@@ -1,29 +1,31 @@
-function SwipeCarousel(){
-  Carousel.apply(this, arguments)
-  this.slidesContainer = this.container.querySelector('#slides-container')
+import Carousel from './carousel.js'
+
+class SwipeCarousel extends Carousel{
+  constructor(...args){
+  super (...args)
+  this.slidesContainer = this.slideItems[0]?.parentNode
+  }
+  _initListeners (){
+    super._initListeners()
+    this.slidesContainer.addEventListener('touchstart', this._swipeStart.bind(this));
+    this.slidesContainer.addEventListener('mousedown', this._swipeStart.bind(this));
+    this.slidesContainer.addEventListener('touchend', this._swipeEnd.bind(this));
+    this.slidesContainer.addEventListener('mouseup', this._swipeEnd.bind(this));
+  }
+
+  _swipeStart (e) {
+    this.startPosX = e instanceof MouseEvent
+    ? e.pageX 
+    : e.changedTouches[0].pageX
+  }
+
+  _swipeEnd (e) {
+    this.endPosX = e instanceof MouseEvent 
+    ? e.pageX 
+    : e.changedTouches[0].pageX
+    if(this.endPosX - this.startPosX > 50) this.previous()
+    if(this.endPosX - this.startPosX < -50) this.next()
+  }
 }
 
-SwipeCarousel.prototype = Object.create(Carousel.prototype)
-SwipeCarousel.prototype.constructor = SwipeCarousel 
-
-SwipeCarousel.prototype._initListeners = function (){
-  Carousel.prototype._initListeners.apply(this)
-  this.slidesContainer.addEventListener('touchstart', this.swipeStartHandler.bind(this));
-  this.slidesContainer.addEventListener('mousedown', this.swipeStartHandler.bind(this));
-  this.slidesContainer.addEventListener('touchend', this.swipeEndHandler.bind(this));
-  this.slidesContainer.addEventListener('mouseup', this.swipeEndHandler.bind(this));
-}
-
-SwipeCarousel.prototype.swipeStartHandler = function(e) {
-  this.startPosX = e instanceof MouseEvent 
-  ? e.pageX 
-  : e.changedTouches[0].pageX
-},
-
-SwipeCarousel.prototype.swipeEndHandler = function(e) {
-  this.endPosX = e instanceof MouseEvent 
-  ? e.pageX 
-  : e.changedTouches[0].pageX
-  if(this.endPosX - this.startPosX > 50) this.previous()
-  if(this.endPosX - this.startPosX < -50) this.next()
-}
+export default SwipeCarousel
